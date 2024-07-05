4c3e38bcd7c15d8092292f048aa96defa543cf86
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-07-05 11:22:26 -0700

    Test CI/CD pipeline

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("This is a test", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("#test").contains("This is a test");
+  });
+});
