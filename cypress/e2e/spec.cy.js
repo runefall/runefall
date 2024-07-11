1771d3ef7e88eb008537b4f2d3a2766e6b76ea2e
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-07-11 15:41:02 -0700

    Style homepage

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,8 @@
 describe("template spec", () => {
   it("This is a test", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("#test").contains("This is a test.");
+    cy.getTestId("slogan").contains(
+      "Runefall is a powerful Legends of Runeterra card search",
+    );
   });
 });
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,44 @@
-import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { useEffect, useRef, useState } from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
 
+  const searchBar = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    searchBar.current?.focus();
+  }, []);
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-blue-900">
-      <h1 id="test">This block for testing purposes. Delete later.</h1> {/* Added to pass auto Cypress testing */}
-      <h1 className="text-center text-4xl text-white">
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-b from-blue-900 to-blue-700 p-5">
+      <h1
+        data-test-id="slogan"
+        className="mb-8 text-center text-4xl text-white"
+      >
         <b className="font-bold">Runefall</b> is a powerful{" "}
         <b className="font-bold">Legends of Runeterra</b> card search
       </h1>
-      <input
-        className="bg-black text-white"
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.code == "Enter") {
-            navigate(`/search?query=${search}`);
-          }
-        }}
-      />
+      <div className="relative w-full max-w-[600px]">
+        <FaMagnifyingGlass
+          size={"1.5rem"}
+          className="absolute left-4 top-1/2 -translate-y-1/2"
+        />
+        <Input
+          ref={searchBar}
+          className="h-16 w-full rounded-none pl-12 text-xl"
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.code == "Enter")
+              navigate(`/search?query=${encodeURIComponent(search.trim())}`);
+          }}
+        />
+      </div>
     </div>
   );
 }
