088ff713594b92826a7a2a9f00fc6f05d1f163ee
Theo McCray <tlmccraycray@gmail.com>
2024-07-11 19:52:47 -0600

    minor adjustments

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
 
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-blue-900">
-      <h1 id="test">This block for testing purposes. Delete later.</h1> {/* Added to pass auto Cypress testing */}
+      <h1 id="test">This block for testing purposes. Delete later.</h1> {/* Added to pass auto Cypress testing for CI*/}
       <h1 className="text-center text-4xl text-white">
         <b className="font-bold">Runefall</b> is a powerful{" "}
         <b className="font-bold">Legends of Runeterra</b> card search
