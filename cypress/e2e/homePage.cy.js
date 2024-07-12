c4f5ae2153025f79e63a22828c4686a2124d5f87
NeilTheSeal <neil.hendren@gmail.com>
2024-07-12 13:48:19 -0600

    Add e2e tests for search parameter persistence to navbar input

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -12,6 +12,7 @@ describe("template spec", () => {
 
     cy.getTestId("home-search-bar").focused().type("draven").type("{enter}");
     cy.url().should("include", `${baseUrl}/search?query=draven`);
+    cy.getTestId("nav-search-bar-input").should("have.value", "draven");
 
     cy.visit("/");
     cy.getTestId("home-search-bar")
@@ -20,6 +21,10 @@ describe("template spec", () => {
     cy.url().should(
       "include",
       `${baseUrl}/search?query=Draven%27s%20Biggest%20Fan`,
+    );
+    cy.getTestId("nav-search-bar-input").should(
+      "have.value",
+      "Draven's Biggest Fan",
     );
   });
 });
