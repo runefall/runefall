eaaf8db2b234d22294f62a1c0b5b7a1c07268f77
Theo M <136327208+Virulencies@users.noreply.github.com>
2024-07-12 12:45:30 -0600

    Update types.ts
    adding associatedcard types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,39 +1,69 @@
 export interface Card {
-    id: string;
-    type: string;
-    attributes: CardAttributes;
-  }
-  
-  export interface CardAttributes {
-    name: string;
-    card_code: string;
-    description: string;
-    description_raw: string;
-    levelup_description: string;
-    levelup_description_raw: string;
-    flavor_text: string;
-    artist_name: string;
-    attack: number;
-    cost: number;
-    health: number;
-    spell_speed: string;
-    rarity: string;
-    supertype: string;
-    card_type: string;
-    collectible: boolean;
-    set: string;
-    associated_card_refs: string[];
-    regions: string[];
-    region_refs: string[];
-    keywords: string[];
-    keyword_refs: string[];
-    formats: string[];
-    format_refs: string[];
-    assets: CardAsset[];
-    associated_cards: Card[];
-  }
-  
-  export interface CardAsset {
-    game_absolute_path: string;
-    full_absolute_path: string;
-  }
+  id: string;
+  type: string;
+  attributes: CardAttributes;
+}
+
+export interface CardAttributes {
+  name: string;
+  card_code: string;
+  description: string;
+  description_raw: string;
+  levelup_description: string;
+  levelup_description_raw: string;
+  flavor_text: string;
+  artist_name: string;
+  attack: number;
+  cost: number;
+  health: number;
+  spell_speed: string;
+  rarity: string;
+  supertype: string;
+  card_type: string;
+  collectible: boolean;
+  set: string;
+  associated_card_refs: string[];
+  regions: string[];
+  region_refs: string[];
+  keywords: string[];
+  keyword_refs: string[];
+  formats: string[];
+  format_refs: string[];
+  assets: CardAsset[];
+  associated_cards: AssociatedCard[];
+}
+
+export interface CardAsset {
+  game_absolute_path: string;
+  full_absolute_path: string;
+}
+
+export interface AssociatedCard {
+  id: string;
+  name: string;
+  card_code: string;
+  description: string;
+  description_raw: string;
+  levelup_description: string;
+  levelup_description_raw: string;
+  flavor_text: string;
+  artist_name: string;
+  attack: number;
+  cost: number;
+  health: number;
+  spell_speed: string;
+  rarity: string;
+  supertype: string;
+  card_type: string;
+  collectible: boolean;
+  set: string;
+  associated_card_refs: string[];
+  regions: string[];
+  region_refs: string[];
+  keywords: string[];
+  keyword_refs: string[];
+  formats: string[];
+  format_refs: string[];
+  assets: CardAsset[];
+  associated_cards: AssociatedCard[];
+}
