cef2595f73baf21a374eb940263b435932974924
laurel Bonal <laurelbonal@gmail.com>
2024-07-25 15:34:35 -0600

    Fix merge conflict

--- src/pages/FallBackPage.tsx
+++ src/pages/FallBackPage.tsx
@@ -1,10 +1,9 @@
-import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
-import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 import { useErrorBoundary } from "react-error-boundary";
-import { TbFaceIdError } from "react-icons/tb";
 import { useNavigate } from "react-router-dom";
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import { TbFaceIdError } from "react-icons/tb";
 
 export default function FallBackPage({ error }: { error: Error | null }) {
   const { resetBoundary } = useErrorBoundary();
@@ -22,26 +21,22 @@ export default function FallBackPage({ error }: { error: Error | null }) {
   };
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex flex-col min-h-screen">
       <NavBar />
       <div
         className="flex flex-1 flex-col items-center justify-center px-4 py-8"
         role="alert"
       >
-        <div className="shadow-shadow max-w-lg rounded-lg p-6 text-center shadow-[0_0_15px_5px]">
-          <TbFaceIdError
-            size={"7rem"}
-            className="mx-auto mb-4 text-muted-foreground"
-          />
-          <div data-test-id="error-message">
-            <h1 className="mb-2 text-xl font-semibold">
-              Something Went Wrong:
-            </h1>
-            <p className="mb-4">{error.message}</p>
-          </div>
-          <Button onClick={handleReset} data-test-id="error-reset-button">
+        <div className="shadow-center-md rounded-lg p-6 text-center max-w-lg dark:shadow-dark-center-md">
+          <TbFaceIdError size={"7rem"} className="text-muted-foreground mx-auto mb-4 dark:text-dark-muted-foreground" />
+          <h1 className="text-xl font-semibold mb-2">Something Went Wrong:</h1>
+          <p className="mb-4">{error ? error.message : "Unknown error"}</p>
+          <button
+            onClick={handleReset}
+            className="bg-primary text-primary-foreground py-2 px-4 rounded hover:bg-primary-600 focus:outline-none dark:bg-dark-primary dark:text-dark-primary-foreground dark:hover:bg-dark-primary-600"
+          >
             Go Back
-          </Button>
+          </button>
         </div>
       </div>
       <Footer />
