77867dcea120eeb214666844ff473e9b17fa2ef8
Billy Wallace <wallace.billy1@gmail.com>
2024-07-30 11:04:29 -0600

    fix color issue for example boxes

--- src/components/Syntax.tsx
+++ src/components/Syntax.tsx
@@ -1,18 +1,21 @@
 import { useNavigate } from 'react-router-dom';
+import { cn } from '@/lib/utils';
 
 interface SyntaxProps {
   title: string;
   description: string;
   examples: string[];
+  color: string;
 }
 
-const Syntax: React.FC<SyntaxProps> = ({ title, description, examples }) => {
+const Syntax: React.FC<SyntaxProps> = ({ title, description, examples, color }) => {
   const navigate = useNavigate();
 
   const handleExampleClick = (example: string) => {
     navigate(`/search?query=${example}`);
   };
-  
+
+  console.log(color)
   return (
     <div className="p-8 flex flex-col sm:flex-row border-t border-black">
       <div className="sm:w-1/2 sm:p-4 sm:pr-20 text-center sm:text-left">
@@ -22,13 +25,14 @@ const Syntax: React.FC<SyntaxProps> = ({ title, description, examples }) => {
       <div className="sm:w-1/2 flex items-center">
         <div className="w-full">
           {examples.map((example, index) => (
-            <pre
+            <div
               key={index}
-              className="p-3 pl-6 mb-2 shadow-xl relative cursor-pointer before:absolute before:left-0 before:top-0 before:h-full before:w-2 before:bg-blue-500 hover:scale-105 hover:cursor-pointer"
+              className={ 'p-3 pl-6 mb-2 shadow-xl relative cursor-pointer border-l-8 hover:scale-105 hover:cursor-pointer' }
+              style={{ borderColor: color }}
               onClick={() => handleExampleClick(example)}
             >
               {example}
-            </pre>
+            </div>
           ))}
         </div>
       </div>
