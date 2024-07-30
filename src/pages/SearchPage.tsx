a6a2a01fc4a963facc3649dbc167ca01dcb41b7b
laurel Bonal <laurelbonal@gmail.com>
2024-07-30 17:55:51 -0600

    Correct merge conflict

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,11 +2,7 @@ import CardDisplay from "@/components/display/CardDisplay";
 import SearchFilter from "@/components/SearchFilter/SearchFilter";
 import { Card as CardType, FilterState } from "@/types/interfaces";
 import { querySearch } from "@/utils/apiCalls";
-import {
-  isSortAttributeType,
-  isSortDirectionType,
-  isSortModeType,
-} from "@/utils/isType";
+import { isSortAttributeType, isSortDirectionType, isSortModeType } from "@/utils/isType";
 import { calculateRarity } from "@/utils/rarity";
 import { useEffect, useState } from "react";
 import { useErrorBoundary } from "react-error-boundary";
@@ -29,18 +25,20 @@ export default function SearchPage() {
   const { sortMode, sortAttribute, sortDirection } = filterState;
 
   function handleFilterState(action: { type: string; value: string }) {
+    const newParams = new URLSearchParams(searchParams.toString());
     switch (action.type) {
       case "sortMode":
-        searchParams.set("mode", action.value);
+        newParams.set("mode", action.value);
         break;
       case "sortAttribute":
-        searchParams.set("attribute", action.value);
+        newParams.set("attribute", action.value);
         break;
       case "sortDirection":
-        searchParams.set("direction", action.value);
+        newParams.set("direction", action.value);
+        break;
     }
-    searchParams.sort();
-    setSearchParams(searchParams);
+    newParams.sort();
+    setSearchParams(newParams);
   }
 
   useEffect(() => {
@@ -96,65 +94,63 @@ export default function SearchPage() {
     setHasMore(newLength < cards.length);
   };
 
+  const sortCards = (cardsToSort: CardType[], attribute: string, direction: string): CardType[] => {
+    let sortedCards = [...cardsToSort];
+
+    switch (attribute) {
+      case "name":
+      case "card_type":
+      case "set":
+      case "artist_name":
+      case "card_code":
+        sortedCards = sortedCards.sort((card1, card2) =>
+          card1.attributes[attribute].localeCompare(card2.attributes[attribute])
+        );
+        break;
+      case "region_refs":
+        sortedCards = sortedCards.sort((card1, card2) =>
+          card1.attributes[attribute]
+            .join(", ")
+            .localeCompare(card2.attributes[attribute].join(", "))
+        );
+        break;
+      case "attack":
+      case "cost":
+      case "health":
+        sortedCards = sortedCards.sort(
+          (card1, card2) =>
+            card1.attributes[attribute] - card2.attributes[attribute]
+        );
+        break;
+      case "rarity":
+        sortedCards = sortedCards.sort(
+          (card1, card2) =>
+            calculateRarity(card1.attributes.rarity) -
+            calculateRarity(card2.attributes.rarity)
+        );
+        break;
+      default:
+        break;
+    }
+
+    if (direction === "descending") sortedCards.reverse();
+
+    return sortedCards;
+  };
+
+  useEffect(() => {
+    const sortedCards = sortCards(cards, sortAttribute, sortDirection);
+    setDisplayedCards(sortedCards.slice(0, displayedCards.length));
+  }, [sortAttribute, sortDirection]);
+
   if (cards.length === 1) {
     const { card_code } = cards[0].attributes;
     navigate(`/card/${card_code}`);
   }
 
-  let cardsSorted = displayedCards.sort((card1, card2) =>
-    card1.attributes.name.localeCompare(card2.attributes.name)
-  );
-  switch (sortAttribute) {
-    case "name":
-    case "card_type":
-    case "set":
-    case "artist_name":
-    case "card_code":
-    default:
-      cardsSorted = displayedCards.sort((card1, card2) =>
-        card1.attributes[sortAttribute].localeCompare(
-          card2.attributes[sortAttribute]
-        )
-      );
-
-      if (sortDirection === "descending") cardsSorted = cardsSorted.reverse();
-      break;
-    case "region_refs":
-      cardsSorted = displayedCards.sort((card1, card2) =>
-        card1.attributes[sortAttribute]
-          .join(", ")
-          .localeCompare(card2.attributes[sortAttribute].join(", "))
-      );
-
-      if (sortDirection === "descending") cardsSorted = cardsSorted.reverse();
-      break;
-    case "attack":
-    case "cost":
-    case "health":
-      cardsSorted = displayedCards.sort(
-        (card1, card2) =>
-          card1.attributes[sortAttribute] - card2.attributes[sortAttribute]
-      );
-
-      if (sortDirection === "descending") cardsSorted = cardsSorted.reverse();
-      break;
-    case "rarity":
-      cardsSorted = cards.sort(
-        (card1, card2) =>
-          calculateRarity(card1.attributes.rarity) -
-          calculateRarity(card2.attributes.rarity),
-      );
-
-      if (sortDirection === "descending") cardsSorted = cardsSorted.reverse();
-      break;
-  }
-
   return (
     <>
-      <SearchFilter
-        filterState={filterState}
-        setFilterState={handleFilterState}
-      />
+      <SearchFilter filterState={filterState} setFilterState={handleFilterState} />
       {cards.length === 0 ? (
         <div data-test-id="no-cards" className="m-8 w-auto text-center">
           No cards found with the specified search query.
