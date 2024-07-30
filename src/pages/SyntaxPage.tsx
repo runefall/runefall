58ea10f5ac77a8fdfb212bed8b16c359b9a9b5a8
Billy Wallace <wallace.billy1@gmail.com>
2024-07-30 12:12:59 -0600

    Update formatting

--- src/pages/SyntaxPage.tsx
+++ src/pages/SyntaxPage.tsx
@@ -13,17 +13,19 @@ export default function SyntaxPage() {
       <p className="mb-8 text-center">
         Runefall includes a large set of keywords and expressions you can use to filter Legends of Runeterra cards.
       </p>
-      {syntaxData.map((syntaxEntry, index) => {
-        return (
-          <Syntax
-            key={index}
-            title={syntaxEntry.title}
-            description={syntaxEntry.description}
-            examples={syntaxEntry.examples}
-            color={syntaxEntry.color}
-          />
-        );
-      })}
+      <div data-test-id="syntax-entry">
+        {syntaxData.map((syntaxEntry, index) => {
+          return (
+            <Syntax
+              key={index}
+              title={syntaxEntry.title}
+              description={syntaxEntry.description}
+              examples={syntaxEntry.examples}
+              color={syntaxEntry.color}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
