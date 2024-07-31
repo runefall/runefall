d3aa79d07d7ea3cf942bd47496516495a3825a1d
laurel Bonal <laurelbonal@gmail.com>
2024-07-31 16:37:46 -0600

    fix code so cypress passes

--- src/components/display/CardDisplay.tsx
+++ src/components/display/CardDisplay.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
 import { Card as CardType } from "@/types/interfaces";
 import { SortMode } from "@/types/types";
-import { Link } from "react-router-dom";
+import { TbDeviceIpadQuestion } from "react-icons/tb";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
 import CardFull from "./CardFull";
 import CardImage from "./CardImage";
 import CardList from "./CardList";
@@ -11,108 +11,98 @@ import CardText from "./CardText";
 export default function CardDisplay({
   mode,
   cards,
-  itemsPerPage = 20
+  loading = false,
 }: {
   mode: SortMode;
   cards: CardType[];
-  itemsPerPage?: number;
+  loading?: boolean;
 }) {
-  const [displayedCards, setDisplayedCards] = useState<CardType[]>([]);
-  const [hasMore, setHasMore] = useState(true);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    setDisplayedCards(cards.slice(0, itemsPerPage));
-    setHasMore(cards.length > itemsPerPage);
-  }, [cards]);
-
-  const fetchMoreData = () => {
-    const currentLength = displayedCards.length;
-    const newLength = currentLength + itemsPerPage;
-    const newCards = cards.slice(0, newLength);
-    setDisplayedCards(newCards);
-    setHasMore(newLength < cards.length);
-  };
+  if (loading) {
+    return (
+      <div
+        data-test-id="no-cards"
+        className="flex flex-1 items-center justify-center"
+      >
+        Loading ...
+      </div>
+    );
+  } else if (cards.length === 0) {
+    return (
+      <div
+        data-test-id="no-cards"
+        className="flex min-h-[500px] flex-1 items-center justify-center"
+      >
+        <div className="m-4 flex max-w-lg flex-col items-center gap-4 rounded-lg p-6 text-center shadow-[0_0_15px_5px] shadow-shadow">
+          <TbDeviceIpadQuestion
+            size="150px"
+            className="text-muted-foreground"
+          />
+          <h2 className="text-xl font-semibold">No cards found</h2>
+          <p>
+            Your search didn’t match any cards. Adjust your terms or try one of
+            the links below:
+          </p>
+          <div className="flex gap-4">
+            <Button variant="outline" onClick={() => navigate("/")}>
+              Go back
+            </Button>
+            <Button variant="outline" onClick={() => navigate("/syntax")}>
+              Syntax Guide
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   switch (mode) {
     default:
     case "image":
+      const imageElements = cards.map((card, index) => (
+        <Link to={`/card/${card.attributes.card_code}`} key={index}>
+          <CardImage card={card.attributes} />
+        </Link>
+      ));
+
       return (
-        <InfiniteScroll
-          dataLength={displayedCards.length}
-          next={fetchMoreData}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b>Yay! You have seen it all</b>
-            </p>
-          }
-        >
-          <div className="m-4 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] justify-center justify-items-center gap-4">
-            {displayedCards.map((card, index) => (
-              <Link to={`/card/${card.attributes.card_code}`} key={index}>
-                <CardImage card={card.attributes} />
-              </Link>
-            ))}
-          </div>
-        </InfiniteScroll>
+        <div className="m-4 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] justify-center justify-items-center gap-4">
+          {imageElements}
+        </div>
       );
     case "text":
-      return (
-        <InfiniteScroll
-          dataLength={displayedCards.length}
-          next={fetchMoreData}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b>Yay! You have seen it all</b>
-            </p>
-          }
+      const textElements = cards.map((card, index) => (
+        <Link
+          to={`/card/${card.attributes.card_code}`}
+          key={index}
+          className="flex w-full justify-center"
         >
-          <div className="m-4 grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-center justify-items-center gap-2 gap-x-4 gap-y-8">
-            {displayedCards.map((card, index) => (
-              <Link
-                to={`/card/${card.attributes.card_code}`}
-                key={index}
-                className="flex w-full justify-center"
-              >
-                <CardText card={card.attributes} />
-              </Link>
-            ))}
-          </div>
-        </InfiniteScroll>
+          <CardText card={card.attributes} />
+        </Link>
+      ));
+
+      return (
+        <div className="m-4 grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] justify-center justify-items-center gap-2 gap-x-4 gap-y-8">
+          {textElements}
+        </div>
       );
     case "list":
       return (
         <div className="flex overflow-x-scroll lg:justify-center">
-          <CardList cards={displayedCards} />
+          <CardList cards={cards} />
         </div>
       );
     case "full":
-      return (
-        <InfiniteScroll
-          dataLength={displayedCards.length}
-          next={fetchMoreData}
-          hasMore={hasMore}
-          loader={<h4>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b>Yay! You have seen it all</b>
-            </p>
-          }
+      const fullElements = cards.map((card, index) => (
+        <div
+          key={index}
+          className="relative flex w-full flex-col items-center border-b border-border p-12"
         >
-          <div>
-            {displayedCards.map((card, index) => (
-              <div
-                key={index}
-                className="relative flex w-full flex-col items-center border-b border-border p-12"
-              >
-                <CardFull card={card.attributes} />
-              </div>
-            ))}
-          </div>
-        </InfiniteScroll>
-      );
+          <CardFull card={card.attributes} />
+        </div>
+      ));
+
+      return <div>{fullElements}</div>;
   }
 }
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,12 +2,17 @@ import CardDisplay from "@/components/display/CardDisplay";
 import SearchFilter from "@/components/SearchFilter/SearchFilter";
 import { Card as CardType, FilterState } from "@/types/interfaces";
 import { querySearch } from "@/utils/apiCalls";
-import { isSortAttributeType, isSortDirectionType, isSortModeType } from "@/utils/isType";
+import {
+  isSortAttributeType,
+  isSortDirectionType,
+  isSortModeType,
+} from "@/utils/isType";
 import { calculateRarity } from "@/utils/rarity";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useErrorBoundary } from "react-error-boundary";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import BackToTopButton from "@/components/BackToTopButton";
+import { useScrollToTop } from "@/utils/useScrollToTop";
 
 export default function SearchPage() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -17,26 +22,24 @@ export default function SearchPage() {
     sortAttribute: "name",
     sortDirection: "auto",
   });
-  const [showTopButton, setShowTopButton] = useState(false);
+  const loading = useRef(true);
   const navigate = useNavigate();
   const { showBoundary } = useErrorBoundary();
   const { sortMode, sortAttribute, sortDirection } = filterState;
 
   function handleFilterState(action: { type: string; value: string }) {
-    const newParams = new URLSearchParams(searchParams.toString());
     switch (action.type) {
       case "sortMode":
-        newParams.set("mode", action.value);
+        searchParams.set("mode", action.value);
         break;
       case "sortAttribute":
-        newParams.set("attribute", action.value);
+        searchParams.set("attribute", action.value);
         break;
       case "sortDirection":
-        newParams.set("direction", action.value);
-        break;
+        searchParams.set("direction", action.value);
     }
-    newParams.sort();
-    setSearchParams(newParams);
+    searchParams.sort();
+    setSearchParams(searchParams);
   }
 
   useEffect(() => {
@@ -61,95 +64,69 @@ export default function SearchPage() {
     });
 
     setCards([]);
+    loading.current = true;
     querySearch(query)
       .then((data: { data: CardType[] }) => {
-        const sortedCards = sortCards(data.data, sortAttribute, sortDirection);
-        setCards(sortedCards);
+        setCards(data.data);
+        loading.current = false;
       })
       .catch(showBoundary);
   }, [searchParams]);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const handleScroll = () => {
-    if (window.scrollY > 400) {
-      setShowTopButton(true);
-    } else {
-      setShowTopButton(false);
-    }
-  };
-
-  const sortCards = (cardsToSort: CardType[], attribute: string, direction: string): CardType[] => {
-    let sortedCards = [...cardsToSort];
-
-    switch (attribute) {
-      case "name":
-      case "card_type":
-      case "set":
-      case "artist_name":
-      case "card_code":
-        sortedCards = sortedCards.sort((card1, card2) =>
-          card1.attributes[attribute].localeCompare(card2.attributes[attribute])
-        );
-        break;
-      case "region_refs":
-        sortedCards = sortedCards.sort((card1, card2) =>
-          card1.attributes[attribute]
-            .join(", ")
-            .localeCompare(card2.attributes[attribute].join(", "))
-        );
-        break;
-      case "attack":
-      case "cost":
-      case "health":
-        sortedCards = sortedCards.sort(
-          (card1, card2) =>
-            card1.attributes[attribute] - card2.attributes[attribute]
-        );
-        break;
-      case "rarity":
-        sortedCards = sortedCards.sort(
-          (card1, card2) =>
-            calculateRarity(card1.attributes.rarity) -
-            calculateRarity(card2.attributes.rarity)
-        );
-        break;
-      default:
-        break;
-    }
-
-    if (direction === "descending") sortedCards.reverse();
-
-    return sortedCards;
-  };
-
-  useEffect(() => {
-    const sortedCards = sortCards(cards, sortAttribute, sortDirection);
-    setCards(sortedCards);
-  }, [sortAttribute, sortDirection]);
 
   if (cards.length === 1) {
     const { card_code } = cards[0].attributes;
     navigate(`/card/${card_code}`);
   }
 
+  let cardsSorted = cards.sort((card1, card2) =>
+    card1.attributes.name.localeCompare(card2.attributes.name)
+  );
+  switch (sortAttribute) {
+    case "name":
+    case "card_type":
+    case "set":
+    case "artist_name":
+    case "card_code":
+    default:
+      cardsSorted = cards.sort((card1, card2) =>
+        card1.attributes[sortAttribute].localeCompare(card2.attributes[sortAttribute])
+      );
+      if (sortDirection === "descending") cardsSorted = cardsSorted.reverse();
+      break;
+    case "region_refs":
+      cardsSorted = cards.sort((card1, card2) =>
+        card1.attributes[sortAttribute]
+          .join(", ")
+          .localeCompare(card2.attributes[sortAttribute].join(", "))
+      );
+      if (sortDirection === "descending") cardsSorted = cardsSorted.reverse();
+      break;
+    case "attack":
+    case "cost":
+    case "health":
+      cardsSorted = cards.sort(
+        (card1, card2) =>
+          card1.attributes[sortAttribute] - card2.attributes[sortAttribute]
+      );
+      if (sortDirection === "descending") cardsSorted = cardsSorted.reverse();
+      break;
+    case "rarity":
+      cardsSorted = cards.sort(
+        (card1, card2) =>
+          calculateRarity(card1.attributes.rarity) -
+          calculateRarity(card2.attributes.rarity)
+      );
+      if (sortDirection === "descending") cardsSorted = cardsSorted.reverse();
+      break;
+  }
+
+  const showTopButton = useScrollToTop();
+
   return (
-    <>
+    <div className="flex flex-1 flex-col">
       <SearchFilter filterState={filterState} setFilterState={handleFilterState} />
-      {cards.length === 0 ? (
-        <div data-test-id="no-cards" className="m-8 w-auto text-center">
-          No cards found with the specified search query.
-        </div>
-      ) : (
-        <CardDisplay
-          cards={cards}
-          mode={sortMode}
-        />
-      )}
+      <CardDisplay cards={cardsSorted} mode={sortMode} loading={loading.current} />
       <BackToTopButton show={showTopButton} />
-    </>
+    </div>
   );
 }
